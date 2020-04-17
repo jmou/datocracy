947a286381e7453bfccb23eb0b97272002bd9953
Joe Mou <dev@mou.fo>
2020-04-17 15:54:10 -0400

    initial scrape for systrom.com covid-19 tracker

---
+++ systrom/parse.py
@@ -0,0 +1,30 @@
+import csv
+import re
+import sys
+from datetime import datetime
+
+import wikitextparser as wtp
+
+
+parsed = wtp.parse(sys.stdin.read())
+assert len(parsed.tables) == 2
+assert 'Deaths' in parsed.tables[1].caption
+
+for out, table in zip(sys.argv[1:], parsed.tables):
+    writer = csv.writer(open(out, 'w'))
+    data = table.data()
+
+    header = data[1]
+    assert header[56] == 'Date'
+    header = [re.search(r'\b[A-Z]{2}\b', h).group()  # extract state
+              for h in header[1:56]]
+    header.insert(0, 'Date')
+    writer.writerow(header)
+
+    for row in data[2:]:
+        if row[0] == 'Total':
+            break
+        row = row[:56]
+        date = re.search(r'\b[A-Z][a-z]{2}\b \d\d?', row[0]).group() + ' 2020'
+        row[0] = datetime.strptime(date, '%b %d %Y').date().isoformat()
+        writer.writerow(row)
